609a4eabf8506b38c19648defba38e27e7bfb13a
Anupam Gogoi <c059237@yara.com>
2023-10-28 10:08:39 -0300

    initial

--- pkg/pipeline/defaultpipeline.go
+++ pkg/pipeline/defaultpipeline.go
@@ -2,11 +2,11 @@ package pipeline
 
 import (
 	"github.com/cloudintegrator/go-sonic-core/api/core"
-	"log"
+	"log/slog"
 )
 
 func BuildPipeline(a *core.SonicApp) {
-	log.Printf("********** App starting ********** %s", a.App.Name)
+	slog.Debug("********** App starting ********** %s", a.App.Name, nil)
 	for i := 0; i < len(a.App.Flows); i++ {
 		f := a.App.Flows[i]
 		InitializeFlowComponents(&f)
@@ -17,10 +17,24 @@ func InitializeFlowComponents(flow *core.Flow) {
 	if len(flow.Components) != 0 {
 		for i := 0; i < len(flow.Components); i++ {
 			component := flow.Components[i]
-			log.Printf("********** Initilizing Component ********** %s", component.Name)
+			slog.Debug("********** Initilizing Component ********** %s", component.Name, nil)
 
 			component_config := component.Configuration
-			log.Printf("********** Initilizing Component Configuratio ********** %s", component_config)
+
+			slog.Debug("********** Initilizing Component Configuratio ********** %s", component_config, nil)
 		}
 	}
+}
+
+func GetReferenceConfiguration(component string, a *core.SonicApp) (out interface{}) {
+	slog.Debug("********** Get reference configuration for: %s", component, nil)
+	if len(a.App.Common) != 0 {
+		for i := 0; i < len(a.App.Common); i++ {
+			config := a.App.Common[0]
+			if component == config.Name {
+				return config
+			}
+		}
+	}
+	return nil
 }
